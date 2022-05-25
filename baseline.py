eabae08ee61ec765b4a2ad0629807e052c4946a4
quyujunbbb <37011519+quyujunbbb@users.noreply.github.com>
2022-05-25 11:05:09 +0800

    Update codes

--- baseline.py
+++ baseline.py
@@ -12,58 +12,48 @@ from torch import nn
 from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import DataLoader
 
-from utils.prepare_data_bf_ibf import create_mhhri
+# from utils.prepare_data_bf_ibf import create_mhhri
+from utils.mhhri import split_filelists, create_mhhri
 
 
 
 def save_results(res, fold, output_path, trait):
-    csv_path = f'{output_path}/csv/'
-    fig_path = f'{output_path}/figs/'
-    os.makedirs(csv_path, exist_ok=True)
-    os.makedirs(fig_path, exist_ok=True)
+    os.makedirs(f'{output_path}/csv/', exist_ok=True)
 
-    clms = ['epoch', 'train_loss', 'test_loss', 'acc', 'r2']
-    res = pd.DataFrame(res, columns=clms)
-    res.to_csv(csv_path + f'{trait}_fold{fold}.csv', index=False)
+    cols = [
+        'epoch', 'train_loss', 'test_loss', 'acc_r', 'r2', 'acc_c', 'bal_acc',
+        'precision', 'recall', 'f1', 'auc'
+    ]
 
-    fig, ax = plt.subplots()
-    ax.plot(res['epoch'], res['train_loss'], label='train loss')
-    ax.plot(res['epoch'], res['test_loss'], label='test loss')
-    ax.set_xlabel('epochs')
-    ax.set_ylabel('loss')
-    ax.grid()
-    ax.legend()
-    fig.savefig(fig_path + f'{trait}_fold{fold}_loss.png')
-
-    fig, ax = plt.subplots()
-    ax.plot(res['epoch'], res['acc'], label='test accuray')
-    ax.set_xlabel('epochs')
-    ax.set_ylabel('test accuray')
-    ax.grid()
-    ax.legend()
-    fig.savefig(fig_path + f'{trait}_fold{fold}_acc.png')
-
-    smooth_factor = 0.8
-    smooth_train = res['train_loss'].ewm(alpha=(1 - smooth_factor)).mean()
-    smooth_test = res['test_loss'].ewm(alpha=(1 - smooth_factor)).mean()
-
-    fig, ax = plt.subplots()
-    ax.plot(res['epoch'], smooth_train, label='train loss')
-    ax.plot(res['epoch'], smooth_test, label='test loss')
-    ax.set_xlabel('epochs')
-    ax.set_ylabel('loss')
-    ax.grid()
-    ax.legend()
-    fig.savefig(fig_path + f'{trait}_fold{fold}_loss_s.png')
+    res = pd.DataFrame(res, columns=cols)
+    res.to_csv(f'{output_path}/csv/{trait}_fold{fold}.csv', index=False)
 
 
-def evaluate_res_reg(y_true, y_pred):
-    y_true = np.array(y_true)
-    y_pred = np.array(y_pred)
-    acc = np.sum(1 - np.abs(y_true - y_pred)) / len(y_true)
+def evaluate_res(y_true, y_pred, trait, fold, output_path):
+    # save csv results
+    os.makedirs(f'{output_path}/pred/', exist_ok=True)
+    y_true, y_pred = y_true.reshape(-1), y_pred.reshape(-1)
+    y_out = pd.DataFrame(columns=['y_true', 'y_pred'])
+    y_out['y_true'], y_out['y_pred'] = y_true, y_pred
+    y_out.to_csv(f'{output_path}/pred/{trait}_fold{fold}.csv', index=False)
+
+    # evaluate regression results
+    acc_r = 1 - np.sum(np.abs(y_true - y_pred)) / len(y_true)
     r2 = 1 - np.sum((y_true - y_pred)**2) / len(y_true)
 
-    return acc, r2
+    # evaluate classification results
+    y_true_c = np.where(y_true >= 0.5, 1, 0)
+    y_pred_c = np.where(y_pred >= 0.5, 1, 0)
+
+    acc_c = metrics.accuracy_score(y_true_c, y_pred_c)
+    bal_acc = metrics.balanced_accuracy_score(y_true_c, y_pred_c)
+    p, r, f1 = metrics.precision_recall_fscore_support(y_true_c,
+                                                       y_pred_c,
+                                                       average='macro')[:-1]
+    fpr, tpr, _ = metrics.roc_curve(y_true_c, y_pred_c)
+    auc = metrics.auc(fpr, tpr)
+
+    return acc_r, r2, acc_c, bal_acc, p, r, f1, auc
 
 
 def train(task, label_type, trait, output_path):
@@ -75,40 +65,31 @@ def train(task, label_type, trait, output_path):
 
     res_overall = {}
     for fold in range(fold_num):
-        self_body_test_files = self_body_data_list[fold]
-        self_body_train_list = np.delete(self_body_data_list, fold, axis=0)
-        self_body_train_files = [item for row in self_body_train_list for item in row]
+        s_body_train, s_body_test, s_face_train, s_face_test, i_body_train, i_body_test, i_face_train, i_face_test = split_filelists(
+            self_body_data_list, self_face_data_list, interact_body_data_list,
+            interact_face_data_list, fold)
 
-        self_face_test_files = self_face_data_list[fold]
-        self_face_train_list = np.delete(self_face_data_list, fold, axis=0)
-        self_face_train_files = [item for row in self_face_train_list for item in row]
+        train_data, test_data = create_mhhri(s_body_train, s_body_test,
+                                             s_face_train, s_face_test,
+                                             i_body_train, i_body_test,
+                                             i_face_train, i_face_test, task,
+                                             'reg', trait)
 
-        interact_body_test_files = interact_body_data_list[fold]
-        interact_body_train_list = np.delete(interact_body_data_list, fold, axis=0)
-        interact_body_train_files = [item for row in interact_body_train_list for item in row]
-
-        interact_face_test_files = interact_face_data_list[fold]
-        interact_face_train_list = np.delete(interact_face_data_list, fold, axis=0)
-        interact_face_train_files = [item for row in interact_face_train_list for item in row]
-        
-        logger.info(
-            f'fold {fold} - train_num={len(self_body_train_files)}, test_num={len(self_body_test_files)}'
-        )
-
-        train_data, test_data = create_mhhri(
-            self_body_train_files, self_body_test_files,
-            self_face_train_files, self_face_test_files,
-            interact_body_train_files, interact_body_test_files,
-            interact_face_train_files, interact_face_test_files,
-            task, label_type, trait
-        )
-
-        train_loader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
-        test_loader = DataLoader(dataset=test_data, batch_size=BATCH_SIZE, shuffle=True)
+        train_loader = DataLoader(dataset=train_data,
+                                  batch_size=BATCH_SIZE,
+                                  shuffle=True,
+                                  num_workers=NUM_WORKERS)
+        test_loader = DataLoader(dataset=test_data,
+                                 batch_size=BATCH_SIZE,
+                                 shuffle=True,
+                                 num_workers=NUM_WORKERS)
 
         criterion = nn.MSELoss()
 
-        logger.info('epoch   train_l  test_l     acc      r2')
+        logger.info(
+            f'fold    | train_l  test_l |    acc     r2 |    '
+            f'acc  b_acc      p      r     f1    auc'
+        )
         res = []
         for epoch in range(EPOCHS):
             # train
@@ -146,25 +127,49 @@ def train(task, label_type, trait, output_path):
             mean_loss = sum(total_loss) / total_loss.__len__()
             y_true = np.concatenate(true_label_list)
             y_pred = np.concatenate(pred_label_list)
-            acc, r2 = evaluate_res_reg(y_true, y_pred)
+            acc_r, r2, acc_c, bal_acc, p, r, f1, auc = evaluate_res(
+                y_true, y_pred, trait, fold, output_path)
 
             # logs
             logger.info(
-                f'[{epoch+1:02d}/{EPOCHS:02d}] '
-                f'{train_loss.item(): 2.4f} {mean_loss.item(): 2.4f} {acc: 2.4f} {r2: 2.4f}')
-            res.append([epoch, train_loss.item(), mean_loss.item(), acc, r2])
-            res_overall[fold] = [acc, r2]
+                f'fold {fold} | '
+                f'{train_loss.item(): 2.4f} {mean_loss.item(): 2.4f} | '
+                f'{acc_r:.4f} {r2:.4f} | '
+                f'{acc_c:.4f} {bal_acc:.4f} {p:.4f} {r:.4f} {f1:.4f} {auc:.4f}'
+            )
+            res.append([
+                epoch,
+                train_loss.item(),
+                mean_loss.item(), acc_r, r2, acc_c, bal_acc, p, r, f1, auc
+            ])
+            res_overall[fold] = [acc_r, r2, acc_c, bal_acc, p, r, f1, auc]
 
         save_results(res, fold, output_path, trait)
 
-    mean_acc, mean_r2 = 0.0, 0.0
+    mean_acc_r, mean_r2 = 0.0, 0.0
+    mean_acc_c, mean_bal_acc, mean_p, mean_r, mean_f1, mean_auc = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
     for _, value in res_overall.items():
-        mean_acc += value[0]
+        mean_acc_r += value[0]
         mean_r2 += value[1]
-    mean_acc = mean_acc / fold_num
+        mean_acc_c += value[0]
+        mean_bal_acc += value[1]
+        mean_p += value[2]
+        mean_r += value[3]
+        mean_f1 += value[4]
+        mean_auc += value[5]
+    mean_acc_r = mean_acc_r / fold_num
     mean_r2 = mean_r2 / fold_num
+    mean_acc_c = mean_acc_c / fold_num
+    mean_bal_acc = mean_bal_acc / fold_num
+    mean_p = mean_p / fold_num
+    mean_r = mean_r / fold_num
+    mean_f1 = mean_f1 / fold_num
+    mean_auc = mean_auc / fold_num
     logger.info(
-        f'Average: acc={mean_acc:.4f} b_acc={mean_r2:.4f}\n'
+        f'                             avg | '
+        f'{mean_acc_r:.4f} {mean_r2:.4f} | '
+        f'{mean_acc_c:.4f} {mean_bal_acc:.4f} {mean_p:.4f} {mean_r:.4f} '
+        f'{mean_f1:.4f} {mean_auc:.4f}'
     )
 
 
@@ -180,8 +185,9 @@ if __name__ == '__main__':
     label_type = 'reg'  # 'class'
     traits = ['O', 'C', 'E', 'A', 'N']
 
-    EPOCHS = 10
-    BATCH_SIZE = 32
+    EPOCHS = 1
+    BATCH_SIZE = 16
+    NUM_WORKERS = 2
 
     for trait in traits:
         logger.info(
@@ -190,6 +196,6 @@ if __name__ == '__main__':
             f'\n  TASK          : {task}'
             f'\n  TRAIT         : {trait}'
             f'\n  BATCH SIZE    : {BATCH_SIZE}'
-            f'\n  EPOCH         : {EPOCHS}\n'
+            f'\n  EPOCH         : {EPOCHS}'
         )
         train(task, label_type, trait, output_path)
